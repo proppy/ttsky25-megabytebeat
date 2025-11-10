711fae5eaa4d3e2d875c862792940dc3ab696f6b
Johan Euphrosine <proppy@gmail.com>
2025-11-10 23:19:48 +0900

    test: fix pcm output

--- test/test.py
+++ test/test.py
@@ -19,20 +19,20 @@ async def collect_samples(dut, count):
         await RisingEdge(dut.clk) # wait for first clock edge
         try:
             yield (i,
-                   dut.uo_out[0].value*255,
-                   dut.uo_out[7].value*255)
+                   dut.pcm[0].value.to_unsigned(),
+                   dut.pcm[1].value.to_unsigned())
         except ValueError:
             pass  # ignore invalid samples
 
 
 async def write_wavefile(dut):
     """Write a wavefile corresponding to the outputs."""
-    samples = [s async for s in collect_samples(dut, 65536*4)]
-    df = pd.DataFrame.from_records(samples, columns=['t', 'out0', 'out7'], index=['t'])
+    samples = [s async for s in collect_samples(dut, 65536*4*256)]
+    df = pd.DataFrame.from_records(samples, columns=['t', 'pcm0', 'pcm7'], index=['t'])
     df.plot()
-    plt.savefig('tb.png')
-    scipy.io.wavfile.write('tb_out0.wav', 8000, df['out0'][:].to_numpy(dtype=np.uint8))
-    scipy.io.wavfile.write('tb_out7.wav', 8000, df['out7'][:].to_numpy(dtype=np.uint8))    
+    plt.savefig('tb_pcm.png')
+    scipy.io.wavfile.write('tb_pcm0.wav', 8000, df['pcm0'][:].to_numpy(dtype=np.uint8))
+    scipy.io.wavfile.write('tb_pcm7.wav', 8000, df['pcm7'][:].to_numpy(dtype=np.uint8))
 
 
 @cocotb.test()
@@ -51,22 +51,22 @@ async def test_bytebeat(dut):
     dut.uio_in.value = 0xa3 # c=3 d=10
     await ClockCycles(dut.clk, 1)
     dut.rst_n.value = 1 # take out of reset
-    await ClockCycles(dut.clk, 65536)
+    await ClockCycles(dut.clk, 65536*256)
     dut.rst_n.value = 0 # low to reset
     dut.ui_in.value = 0x63  # a=3 b=6
     dut.uio_in.value = 0x92 # c=2 d=9
     await ClockCycles(dut.clk, 1)
     dut.rst_n.value = 1 # take out of reset
-    await ClockCycles(dut.clk, 65536)
+    await ClockCycles(dut.clk, 65536*256)
     dut.rst_n.value = 0 # low to reset
     dut.ui_in.value = 0x57  # a=7 b=5
     dut.uio_in.value = 0x3a # c=10 d=3
     await ClockCycles(dut.clk, 1)
     dut.rst_n.value = 1 # take out of reset
-    await ClockCycles(dut.clk, 65536)
+    await ClockCycles(dut.clk, 65536*256)
     dut.rst_n.value = 0 # low to reset
     dut.ui_in.value = 0x36  # a=6 b=3
     dut.uio_in.value = 0x29 # c=9 d=2
     await ClockCycles(dut.clk, 1)
     dut.rst_n.value = 1 # take out of reset
-    await ClockCycles(dut.clk, 65536)
+    await ClockCycles(dut.clk, 65536*256)

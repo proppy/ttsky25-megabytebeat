306cf51f53dc815e46d6ac3ff6fc2a90c7b16ed6
Johan Euphrosine <proppy@gmail.com>
2025-11-10 21:54:30 +0900

    test: update tests

--- test/test.py
+++ test/test.py
@@ -1,40 +1,72 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
+# Copyright 2025 Google LLC.
 # SPDX-License-Identifier: Apache-2.0
 
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import ClockCycles, RisingEdge
+
+import numpy as np
+import scipy.io.wavfile
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+FREQ = 8000 * 256
+
+
+async def collect_samples(dut, count):
+    for i in range(count):
+        await RisingEdge(dut.clk) # wait for first clock edge
+        try:
+            yield (i,
+                   dut.uo_out[0].value ? 255 : 0,
+                   dut.uo_out[7].value ? 255 : 0)
+        except ValueError:
+            pass  # ignore invalid samples
+
+
+async def write_wavefile(dut):
+    """Write a wavefile corresponding to the outputs."""
+    samples = [s async for s in collect_samples(dut, 65536*4)]
+    df = pd.DataFrame.from_records(samples, columns=['t', 'out0', 'out7'], index=['t'])
+    df.plot()
+    plt.savefig('tb.png')
+    scipy.io.wavfile.write('tb_out0.wav', 8000, df['out0'][:].to_numpy(dtype=np.uint8))
+    scipy.io.wavfile.write('tb_out7.wav', 8000, df['out7'][:].to_numpy(dtype=np.uint8))    
 
 
 @cocotb.test()
-async def test_project(dut):
+async def test_bytebeat(dut):
     dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
+    cocotb.start_soon(write_wavefile(dut))  # collect samples "in the background"
+
+    # Set the clock to 8000hz
+    clock = Clock(dut.clk, (1.0/FREQ), unit="sec")
     cocotb.start_soon(clock.start())
 
-    # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
-
-    dut._log.info("Test project behavior")
-
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
+    dut.ena.value = 1 # enable project
+    dut.rst_n.value = 0 # low to reset
+    dut.ui_in.value = 0x75  # a=5 b=7
+    dut.uio_in.value = 0xa3 # c=3 d=10
     await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    dut.rst_n.value = 1 # take out of reset
+    await ClockCycles(dut.clk, 65536)
+    dut.rst_n.value = 0 # low to reset
+    dut.ui_in.value = 0x63  # a=3 b=6
+    dut.uio_in.value = 0x92 # c=2 d=9
+    await ClockCycles(dut.clk, 1)
+    dut.rst_n.value = 1 # take out of reset
+    await ClockCycles(dut.clk, 65536)
+    dut.rst_n.value = 0 # low to reset
+    dut.ui_in.value = 0x57  # a=7 b=5
+    dut.uio_in.value = 0x3a # c=10 d=3
+    await ClockCycles(dut.clk, 1)
+    dut.rst_n.value = 1 # take out of reset
+    await ClockCycles(dut.clk, 65536)
+    dut.rst_n.value = 0 # low to reset
+    dut.ui_in.value = 0x36  # a=6 b=3
+    dut.uio_in.value = 0x29 # c=9 d=2
+    await ClockCycles(dut.clk, 1)
+    dut.rst_n.value = 1 # take out of reset
+    await ClockCycles(dut.clk, 65536)
